270ff96a1c02551ea242d1cb37d300f5e5793725
imon hossin <imonhossin104@gmail.com>
2021-10-31 23:18:53 +0600

    first

--- src/pages/Firebase/firebase.config.js
+++ src/pages/Firebase/firebase.config.js
@@ -8,19 +8,5 @@ const firebaseConfig = {
     appId: "1:441368082138:web:1c1f354196640d82e217e3"
   };
 
-
-//   const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID
-// };
-// console.log(process.env)
-
-
-
-
   
   export default firebaseConfig ;
