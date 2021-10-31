4b6e199419688acc39fa8b39ab992f820c07fb1d
imon hossin <imonhossin104@gmail.com>
2021-10-31 23:23:49 +0600

    first

--- src/pages/MyOrder/MyOrder.js
+++ src/pages/MyOrder/MyOrder.js
@@ -1,34 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import Order from '../Order/Order';
-// import './MyOrder.css'
-// const MyOrder = () => {
-//     const email = 'hero@gmail.com'
-//     const [orders,setOrders] = useState([]) 
-//     useEffect(()=>{
-//         fetch(`https://infinite-eyrie-27553.herokuapp.com/myOrder/${email}`) 
-//         .then(res=>res.json()) 
-//         .then(data=>setOrders(data))
-//     },[])
-//     console.log(orders)
-//     return (
-//         <div > 
-//             <h3>{orders?.length}</h3>
-//             <div className='card-container'>
-//             {
-//                orders.map(order=><Order 
-//                 key = {order._id}
-//                order= {order} 
-//                ></Order>)  
-//            }
-//             </div>     
-//         </div>
-//     );
-// };
-
-// export default MyOrder;
-
-
-
 
 import React, { useEffect, useState } from 'react';
 import { Row, Spinner } from 'react-bootstrap';
